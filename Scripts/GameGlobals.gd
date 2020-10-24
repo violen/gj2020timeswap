3bfb296852e86534f78d102b50f37f0d4439e628
André Hauser <violen.myst@gmail.com>
2020-10-24 15:51:15 +0200

    Add DoubleJump

--- Scripts/GameGlobals.gd
+++ Scripts/GameGlobals.gd
@@ -1,7 +1,13 @@
 extends Node
 
+## GAME
+
 var isGameOver = false
 var platform = OS.get_name()
+
+## PROGRESS
+
+var canDoubleJump = false
 
 func _is_mobile() -> bool:
     match platform:
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,6 +5,8 @@ export var jump_force = 600
 export var gravity = 1000
 var velocity = Vector2.ZERO
 
+var is_mid_air = false
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -16,8 +18,9 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(delta):
-#     pass
+func _process(delta):
+    _get_input()
+    _move_Player(delta)
 
 func _move_Player(delta):
 
@@ -26,8 +29,7 @@ func _move_Player(delta):
     velocity.y += gravity * delta
     velocity = move_and_slide(velocity, Vector2.UP)
 
-    if Input.is_action_just_pressed("jump"):
-        velocity.y = -jump_force
+    _check_and_perform_jump()
 
 func _get_input():
     if Input.is_action_pressed("run_right"):
@@ -35,8 +37,17 @@ func _get_input():
     if Input.is_action_pressed("run_left"):
         velocity.x -= speed_x
 
-func _physics_process(delta):
-    _get_input()
-    _move_Player(delta)
+# func _physics_process(delta):
+#     _get_input()
+#     _move_Player(delta)
 
+func _check_and_perform_jump():
+    if Input.is_action_just_pressed("jump"):
+        if GameGlobals.canDoubleJump && is_mid_air:
+            print("can double jump")
+            velocity.y = -jump_force
+            is_mid_air = false
 
+        if is_on_floor():
+            velocity.y = -jump_force
+            is_mid_air = true
