fba638515639f44ef9781470f7d2d7efc9bfd760
André Hauser <violen.myst@gmail.com>
2020-10-24 15:18:52 +0200

    Fix jump mechanic

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,9 @@
-extends Area2D
+extends KinematicBody2D
 
-export var speed = 250
-
+export var speed_x = 35
+export var jump_force = 600
+export var gravity = 1000
+var velocity = Vector2.ZERO
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -10,22 +12,31 @@ export var speed = 250
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    _move_Player(delta)
+# func _process(delta):
+#     pass
 
 func _move_Player(delta):
-    var velocity = Vector2()
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
 
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * speed
+    # if velocity.length() > 0:
+    #     velocity = velocity.normalized() * speed_x
+    velocity.y += gravity * delta
+    velocity = move_and_slide(velocity, Vector2.UP)
 
-    position += velocity * delta
+    if Input.is_action_just_pressed("jump"):
+        velocity.y = -jump_force
+
+func _get_input():
+    if Input.is_action_pressed("run_right"):
+        velocity.x += speed_x
+    if Input.is_action_pressed("run_left"):
+        velocity.x -= speed_x
+
+func _physics_process(delta):
+    _get_input()
+    _move_Player(delta)
+
 
